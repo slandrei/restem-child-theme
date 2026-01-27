d24c7a38c779184bc7ae38a68fb5750b771d3141
slandrei <andrey_lucian2012@yahoo.com>
2026-01-27 17:17:23 +0200

    added missing files

--- dev-tools/create-block.js
+++ dev-tools/create-block.js
@@ -1,6 +1,10 @@
-const { execSync } = require('child_process');
-const path = require('path');
-const fs = require('fs');
+import { execSync } from 'child_process';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const slug = process.argv[2];
 
